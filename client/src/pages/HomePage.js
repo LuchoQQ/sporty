4b9437da63aa37ffe97c03e2cd9934fae7995303
LuchoQQ <lucianosanchez125@hotmail.com>
2022-09-05 17:19:02 -0300

    user sync

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -12,8 +12,10 @@ const HomePage = () => {
       <HomeCard direction={true} /> */}
       <Grid p="1rem">
         <Image src={banner} objectFit="cover" w="100vw" h="70vh" />
-        <Grid autoFlow='column'>
-          <Flex><Text>a</Text></Flex>
+        <Grid autoFlow="column">
+          <Flex>
+            <Text>a</Text>
+          </Flex>
         </Grid>
       </Grid>
       <Grid p="1rem">
